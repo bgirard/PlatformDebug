815e8950f31ef238863f0292a23052439b8248ba
Benoit Girard <b56girard@gmail.com>
2012-07-23 17:35:08 -0400

    Repack with support nightly

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh bgirard@people.mozilla.org "cd public_html/PlatformDebug && echo Pull \$PWD && git pull"
+ssh bgirard@people.mozilla.org "cd public_html/PlatformDebug && echo Pull \$PWD && git pull && chmod 755 platformdebug.*"
